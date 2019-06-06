767e4139348b77e882ada0e981012b26e3155a66
Torres-ssf <sergio.uft@gmail.com>
2019-06-06 18:08:46 -0300

    Create post model and associate with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 100 }
     validates :username, presence: true, length: { maximum: 30 }
+    has_many :posts
 end
